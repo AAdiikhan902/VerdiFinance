a1569f578a25b96dafd340c7d0f30628cfec5306
AAdiikhan902 <adekh904@gmail.com>
2026-07-22 04:49:28 -0700

    Delete VerdiPeriphery.sol

--- VerdiPeriphery.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity =0.6.6;
-
-// Same idea as VerdiCore.sol: pulls in the original, unmodified
-// Uniswap V2 Router02 so Hardhat compiles it. This is the contract
-// the Verdi frontend will actually call for swaps/add/remove liquidity.
-import "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
